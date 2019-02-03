85d1510bd2411f9984eed768c43c133c5568945b
Drake <Drake@EDEN>
2019-02-03 14:48:44 -0500

    Added github link

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     <div class="container">
     		<div class="row">
+                        <a href="https://github.com/ddherman/Customers-Database.git">Github Repo</a>
     			<h3>PHP CRUD Grid</h3>
     		</div>
 			<div class="row">
